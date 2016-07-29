80e0a22595986ee85821166e5cee77371f7caaa6
platelk <platel.kevin@gmail.com>
2016-07-29 19:34:27 +0200

    server: moving http server related code in server.go

--- main.go
+++ main.go
@@ -7,34 +7,6 @@ import (
 	"strconv"
 )
 
-// LaunchServer will launch a server that will handle client request
-func LaunchServer() {
-	log.Println("Launching http server...")
-
-	http.HandleFunc("/client/register", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("On [/client/register] call")
-		defer r.Body.Close()
-
-		id := RegisterClient(42)
-		w.Write([]byte(strconv.FormatInt(int64(id), 10)))
-	})
-
-	http.HandleFunc("/client/list", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("On [/client/list] call")
-		defer r.Body.Close()
-
-		w.Write([]byte(fmt.Sprint(GetClientList())))
-	})
-
-	http.HandleFunc("/message/send/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("On [/message/send] call")
-		defer r.Body.Close()
-
-	})
-
-	http.ListenAndServe(":8080", nil)
-}
-
 func main() {
 	log.Println("Launching satms...")
 	InitClients()
