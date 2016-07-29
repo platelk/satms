c4f47c1d80d332141b086878cbb478fa9a1e2acd
platelk <platel.kevin@gmail.com>
2016-07-29 19:31:54 +0200

    server: moving all client related code in a dedicated file

---
+++ clients.go
@@ -0,0 +1,48 @@
+// Package which will handle all the operations related to the client as :
+//   - register a new client
+//   - get a specific client
+//   - ...
+package satms
+
+// Client is a convinient representation of a client registered in the service
+type Client struct {
+    id string
+}
+
+
+// client
+var clients map[int]int
+
+// internal counter
+var uniqueID int
+
+// GenerateID create an uniqueID for each new client
+func GenerateID() int {
+	uniqueID++
+	return uniqueID
+}
+
+// RegisterClient add a new client inside the list of already known client
+func RegisterClient(val int) int {
+	id := GenerateID()
+	clients[id] = val
+	return id
+}
+
+// GetClientList return an array with all the client registered
+func GetClientList() []int {
+	clientsID := make([]int, len(clients))
+	i := 0
+	for k := range clients {
+		clientsID[i] = k
+		i++
+	}
+
+	return clientsID
+}
+
+// InitClients initialize the necessary component for client management and registration
+func InitClients() {
+	log.Println("Init clients data...")
+	clients = make(map[int]int)
+}
--- main.go
+++ main.go
@@ -7,43 +7,6 @@ import (
 	"strconv"
 )
 
-// client
-var clients map[int]int
-
-// internal counter
-var uniqueID int
-
-// GenerateID create an uniqueID for each new client
-func GenerateID() int {
-	uniqueID++
-	return uniqueID
-}
-
-// RegisterClient add a new client inside the list of already known client
-func RegisterClient(val int) int {
-	id := GenerateID()
-	clients[id] = val
-	return id
-}
-
-// GetClientList return an array with all the client registered
-func GetClientList() []int {
-	clientsID := make([]int, len(clients))
-	i := 0
-	for k := range clients {
-		clientsID[i] = k
-		i++
-	}
-
-	return clientsID
-}
-
-// InitClients initialize the necessary component for client management and registration
-func InitClients() {
-	log.Println("Init clients data...")
-	clients = make(map[int]int)
-}
-
 // LaunchServer will launch a server that will handle client request
 func LaunchServer() {
 	log.Println("Launching http server...")
