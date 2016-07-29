aee71f18ce278cb5d54694cb300b059add77a48f
platelk <platel.kevin@gmail.com>
2016-07-29 21:35:54 +0200

    client: moving some clients logic from simple function to struct function

--- lib/clients.go
+++ lib/clients.go
@@ -1,38 +1,32 @@
-// Package which will handle all the operations related to the client as :
+// Package satms will handle all the operations related to the client as :
 //   - register a new client
 //   - get a specific client
 //   - ...
 package satms
 
-import "log"
+import (
+	"log"
 
-// Client is a convinient representation of a client registered in the service
-type Client struct {
-	// Id is an unique identifier for the client
-	ID string
-}
+	"golang.org/x/net/websocket"
+)
 
 // client
 var clients map[int]int
 
-// internal counter
-var uniqueID int
-
-// GenerateID create an uniqueID for each new client
-func GenerateID() int {
-	uniqueID++
-	return uniqueID
+// ClientList will simplify management of mutiple client and basic action on this clients
+type ClientList struct {
+	clients map[int]*Client
 }
 
 // RegisterClient add a new client inside the list of already known client
-func RegisterClient(val int) int {
+func (*ClientList) RegisterClient(val int) int {
 	id := GenerateID()
 	clients[id] = val
 	return id
 }
 
 // GetClientList return an array with all the client registered
-func GetClientList() []int {
+func (*ClientList) GetClientIDList() []int {
 	clientsID := make([]int, len(clients))
 	i := 0
 	for k := range clients {
@@ -47,4 +41,31 @@ func GetClientList() []int {
 func InitClients() {
 	log.Println("Init clients data...")
 	clients = make(map[int]int)
+}
+
+// Client is a convinient representation of a client registered in the service
+type Client struct {
+	// Id is an unique identifier for the client
+	ID string
+
+	conn *websocket.Conn
+}
+
+// internal counter
+var uniqueID int
+
+// GenerateID create an uniqueID for each new client
+func GenerateID() int {
+	uniqueID++
+	return uniqueID
+}
+
+// Send a message to the Client
+func (*Client) Send(msg []byte) {
+
+}
+
+// Unregister the client
+func (*Client) Unregister() {
+
 }
