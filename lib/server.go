09d80601b744ccb170f9f87f356d3994e2f4b086
platelk <platel.kevin@gmail.com>
2016-07-30 23:56:47 +0200

    server: Adding /message/send logic through HTTP request

--- lib/server.go
+++ lib/server.go
@@ -39,10 +39,16 @@ func LaunchServer() {
 		w.Write([]byte(fmt.Sprint(clientList.GetClientIDList())))
 	})
 
-	http.HandleFunc("/message/send/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/message/send", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("On [/message/send] call")
 		defer r.Body.Close()
 
+		to, _ := strconv.ParseInt(r.URL.Query().Get("to"), 10, 32)
+		from, _ := strconv.ParseInt(r.URL.Query().Get("from"), 10, 32)
+		body := r.URL.Query().Get("body")
+
+		log.Println("Send to client ", to)
+		clientList.Get(int(to)).Send(&Message{int(from), int(to), body})
 	})
 
 	// WebSocket handler
