993bce3f506c91fb02bfd4a0fd0e2ba9c1153a98
platelk <platel.kevin@gmail.com>
2016-07-31 18:49:29 +0200

    message: Adding Topic field in Message

--- lib/message.go
+++ lib/message.go
@@ -2,6 +2,8 @@ package satms
 
 // Message represent a basic message exchange between clients
 type Message struct {
+	// Topic of the message, use to send/receive a message only for specific group of message
+	Topic string `json:"topic"`
 	// From is the id of the sender of the message
 	From int `json:"from"`
 	// To is a ID of the receiver
